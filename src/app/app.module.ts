ddcc2077034f2b9139b2ea2ea2b73e66a8f7448d
Daniel Whatley <danieljwhatley@gmail.com>
2017-04-29 19:34:30 -0400

    Added @angular/animations for some reason (@angular/material still suckssss)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -6,6 +7,7 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { MapModule } from './map/map.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
